57ef1b16c5118ce85840ecae1ce19934cef8e5ad
dagoua <17826876683@163.com>
2020-06-19 09:22:45 +0800

    axios接口返回拦截

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import App from './App.vue'
+// 默认设置
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000;
+// 返回错误接口拦截
+axios.interceptors.response.use(function(response){
+  let res = response.data;
+  if(res.status == 0){
+    return res.data;
+  }else if(res.status == 10){
+    window.location.href = '/#/login';
+  }else{
+    return res.msg;
+  }
+}, function(error){
+  return Promise.reject(error);; 
+})
+Vue.use(VueAxios, axios);
 Vue.config.productionTip = false
 
 new Vue({
