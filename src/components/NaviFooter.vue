b2b8da44ad630f75ea5d50ccafb55fec869903a0
dagoua <17826876683@163.com>
2020-06-28 09:55:59 +0800

    footer组件

--- src/components/NaviFooter.vue
+++ src/components/NaviFooter.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>
+    <div class="footer">
+        <div class="footer-logo">
+            <img src="/imgs/logo-footer.png" alt="logo">
+            <p>小米商城</p>
+        </div>
+        <div class="footer-link">
+            <a href="javascript:;">dagoua主页</a><span>|</span>
+            <a href="javascript:;">全部课程</a><span>|</span>
+            <a href="javascript:;">MIUI</a><span>|</span>
+            <a href="javascript:;">小米公司儿童信息保护规则</a>
+        </div>
+        <div class="footer-copyright">
+            <p>Copyright © 2004 - 2020  <span class="email">京东JD.com</span> 版权所有</p>
+        </div>
     </div>
 </template>
 <script>
@@ -7,3 +20,42 @@ export default {
     name:'navi-footer'
 }
 </script>
+<style lang="scss" scoped>
+    @import './../assets/scss/config.scss';
+    .footer{
+        text-align: center;
+        background-color: #333333;
+        height: 234px;
+        border-top: 4px solid $colorA;
+        .footer-logo{
+            margin: 46px 0 31px 0;
+            img{
+                width: 53px;
+                height: 36px;
+            }
+            p{
+                color: $colorD;
+                font-size: $fontI;
+                margin-top: 13px;
+            }
+        }
+        .footer-link{
+            a{
+                color: $colorD;
+                font-size: $fontI;
+            }
+            span{
+                color: $colorD;
+                font-size: $fontI;
+                margin: 0 10px;
+            }
+        }
+        .footer-copyright{
+            color: $colorD;
+            font-size: $fontI;
+            .email{
+                color: $colorA;
+            }
+        }
+    }
+</style>
--- src/components/NaviHeader.vue
+++ src/components/NaviHeader.vue
@@ -176,6 +176,7 @@ export default {
                     border-top: 1px solid $colorH;
                     box-shadow:0px 7px 6px 0px rgba(0, 0, 0, 0.11);
                     transition: all .5s;
+                    background-color: #ffffff;
                     .product{
                         position: relative;
                         display: inline-block;
